0fb97f940ae9c85a4e8b6fb7955853208be76e28
mooghen2s <167309023+mooghen2s@users.noreply.github.com>
2024-05-18 06:10:20 +0700

    Delete a.bat

--- a.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-powershell -Command "Invoke-WebRequest https://huggingface.co/vinthony/SadTalker-V002rc/resolve/main/SadTalker_V0.0.2_256.safetensors -OutFile  .\checkpoints\SadTalker_V0.0.2_256.safetensors "
-powershell -Command "Invoke-WebRequest https://huggingface.co/vinthony/SadTalker-V002rc/resolve/main/SadTalker_V0.0.2_512.safetensors -OutFile  .\checkpoints\SadTalker_V0.0.2_512.safetensors "
-powershell -Command "Invoke-WebRequest https://huggingface.co/vinthony/SadTalker-V002rc/resolve/main/epoch_00190_iteration_000400000_checkpoint.pt -OutFile .\checkpoints\epoch_00190_iteration_000400000_checkpoint.pt "
-powershell -Command "Invoke-WebRequest https://huggingface.co/vinthony/SadTalker-V002rc/resolve/main/mapping_00109-model.pth.tar -OutFile .\checkpoints\mapping_00109-model.pth.tar "
-powershell -Command "Invoke-WebRequest https://huggingface.co/vinthony/SadTalker-V002rc/resolve/main/mapping_00229-model.pth.tar -OutFile .\checkpoints\mapping_00229-model.pth.tar "
